3de7bd3a09f903053219727ff215412f851fdd9a
GamePc <GamePc@DESKTOP-EMGR9G1>
2025-02-05 22:32:20 +0300

    Add project files.

---
+++ Core/YoutubeApi.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Application.Bases;
+using YoutubeApi.Application.Features.Products.Rules;
+using YoutubeApi.Application.Interfaces.AutoMapper;
+using YoutubeApi.Application.Interfaces.UnitOfWorks;
+using YoutubeApi.Domain.Entities;
+
+namespace YoutubeApi.Application.Features.Products.Command.CreateProduct
+{
+    public class CreateProductCommandHandler : BaseHandler, IRequestHandler<CreateProductCommandRequest, Unit>
+    {
+        private readonly ProductRules productRules;
+
+        public CreateProductCommandHandler(ProductRules productRules, IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.productRules = productRules;
+        }
+        public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                IList<Product> products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+                await productRules.ProductTitleMustNotBeSame(products, request.Title);
+
+                Product product = new(request.Title, request.Description, request.BrandId, request.Price, request.Discount);
+
+                await unitOfWork.GetWriteRepository<Product>().AddAsync(product);
+                if (await unitOfWork.SaveAsync() > 0)
+                {
+                    var productCategories = new List<ProductCategory>();
+                    foreach (var categoryId in request.CategoryIds)
+                    {
+                        productCategories.Add(new ProductCategory
+                        {
+                            ProductId = product.Id,
+                            CategoryId = categoryId
+                        });
+                    }
+
+                    await unitOfWork.GetWriteRepository<ProductCategory>().AddRangeAsync(productCategories);
+                    await unitOfWork.SaveAsync(); // Tek bir kaydetme işlemi
+                }
+
+                return Unit.Value;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (logging mechanism not shown here)
+                throw new Exception("An error occurred while creating the product.", ex);
+            }
+        }
+    }
+}
---
+++ Infrastructure/YoutubeApi.Persistence/Migrations/20250204134449_init1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YoutubeApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class init1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Name" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(5544), "Industrial, Electronics & Baby" });
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Name" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(5556), "Books, Games & Baby" });
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Name" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(5566), "Grocery, Toys & Music" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(7050));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(7052));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(7053));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 2, 4, 16, 44, 49, 429, DateTimeKind.Local).AddTicks(7055));
+
+            migrationBuilder.UpdateData(
+                table: "Details",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Description", "Title" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 430, DateTimeKind.Local).AddTicks(9291), "Velit quaerat cezbelendi olduğu magni.", "Enim." });
+
+            migrationBuilder.UpdateData(
+                table: "Details",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Description", "Title" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 430, DateTimeKind.Local).AddTicks(9315), "Deleniti sarmal in koştum cesurca.", "Orta aut." });
+
+            migrationBuilder.UpdateData(
+                table: "Details",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Description", "Title" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 430, DateTimeKind.Local).AddTicks(9399), "Aspernatur kulu orta bilgisayarı balıkhaneye.", "Kapının." });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Description", "Discount", "Price", "Title" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 432, DateTimeKind.Local).AddTicks(4412), "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit", 8.600556182779470m, 284.41m, "Intelligent Soft Towels" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Description", "Discount", "Price", "Title" },
+                values: new object[] { new DateTime(2025, 2, 4, 16, 44, 49, 432, DateTimeKind.Local).AddTicks(4433), "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit", 1.897389425426760m, 700.97m, "Sleek Metal Chips" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Name" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 428, DateTimeKind.Local).AddTicks(9516), "Garden & Health" });
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Name" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 428, DateTimeKind.Local).AddTicks(9536), "Jewelery, Sports & Home" });
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Name" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 428, DateTimeKind.Local).AddTicks(9546), "Books & Shoes" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 10, 1, 15, 6, 45, 429, DateTimeKind.Local).AddTicks(2026));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 10, 1, 15, 6, 45, 429, DateTimeKind.Local).AddTicks(2028));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 10, 1, 15, 6, 45, 429, DateTimeKind.Local).AddTicks(2029));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 10, 1, 15, 6, 45, 429, DateTimeKind.Local).AddTicks(2031));
+
+            migrationBuilder.UpdateData(
+                table: "Details",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Description", "Title" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 431, DateTimeKind.Local).AddTicks(4451), "Ekşili sandalye sıla bilgisayarı türemiş.", "Adresini." });
+
+            migrationBuilder.UpdateData(
+                table: "Details",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Description", "Title" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 431, DateTimeKind.Local).AddTicks(4484), "Dignissimos sayfası labore ea eum.", "Reprehenderit consequatur." });
+
+            migrationBuilder.UpdateData(
+                table: "Details",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Description", "Title" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 431, DateTimeKind.Local).AddTicks(4508), "İn gidecekmiş vel enim incidunt.", "Vitae." });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Description", "Discount", "Price", "Title" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 433, DateTimeKind.Local).AddTicks(9143), "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality", 1.499450369695510m, 25.23m, "Sleek Wooden Computer" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Description", "Discount", "Price", "Title" },
+                values: new object[] { new DateTime(2023, 10, 1, 15, 6, 45, 433, DateTimeKind.Local).AddTicks(9169), "The Football Is Good For Training And Recreational Purposes", 0.7931595161475990m, 875.94m, "Tasty Steel Soap" });
+        }
+    }
+}
